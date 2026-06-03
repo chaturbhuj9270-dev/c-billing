cf75fbb91240865b985483d1442e087eab9a008a
chaturbhuj9270-dev <chaturbhuj.9270@gmail.com>
2026-06-03 19:56:47 +0530

    Enable uncompressed native libs and bump deps

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.c_billing"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -41,11 +41,6 @@ android {
         }
     }
 
-    packaging {
-        jniLibs {
-            useLegacyPackaging = true
-        }
-    }
 }
 
 flutter {
